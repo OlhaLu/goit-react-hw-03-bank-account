2256240f30af21efde134e76a7971eba98feca1c
OlhaLu <olhalutsyshyna@gmail.com>
2019-10-25 11:45:10 +0300

    Updates

---
+++ precache-manifest.01b573f59a4785795ef0a3c05fcc2bca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c874d577dfad48e9482aebcbbd27d168",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "4b3b29455400a19d0c0e",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "151154f776e587f03df9",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.08fddb8b.chunk.css"
+  },
+  {
+    "revision": "4b3b29455400a19d0c0e",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.1965da1b.chunk.js"
+  },
+  {
+    "revision": "151154f776e587f03df9",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.b4b2c9c9.chunk.js"
+  },
+  {
+    "revision": "9442ab920daa0b9f3f5e",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime-main.8518e471.js"
+  }
+]);
